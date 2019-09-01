d5c01ea914fee30e2946ab56703d385ecdef458d
Fran <svarcf@gmail.com>
2019-09-01 17:50:00 +0200

    Registration implemented

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,21 +2,57 @@ import React from 'react';
 import {Container, Form, Button} from 'react-bootstrap';
 
 class Registration extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            email: "",
+        };
+
+
+        this.handleRegistration = this.handleRegistration.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleRegistration(event){
+        event.preventDefault();
+        fetch('http://localhost:8082/user/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state)
+        })
+    }
     render() {
         return (
             <Container>
-                <Form>
-                    <Form.Group controlId="formGroupEmail">
+                <Form onSubmit={this.handleRegistration}>
+                    <Form.Group controlId="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleInputChange}/>
                     </Form.Group>
-                    <Form.Group controlId="formGroupUsername">
+                    <Form.Group controlId="username">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Username" />
+                        <Form.Control type="text" placeholder="Username" name="username" value={this.state.username} onChange={this.handleInputChange}/>
                     </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
+                    <Form.Group controlId="password">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleInputChange}/>
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
